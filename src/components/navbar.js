19f10fd1c56dd03e063071c55ad4f36a6001ec3c
matias <matias@ibserveis.com>
2025-04-04 19:10:36 +0200

    navbar component

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+export function Navbar() {
+  const nav = document.createElement("nav");
+  nav.className = "navbar bg-success";
+
+  nav.innerHTML = `
+      <div class="container">
+          <a class="navbar-brand text-white" href="#">InformaticOn</a>
+          <ul class="nav">
+              <li class="nav-item"><a class="nav-link text-white" href="#header">Inicio</a></li>
+              <li class="nav-item"><a class="nav-link text-white" href="#productos">Productos</a></li>
+              <li class="nav-item"><a class="nav-link text-white" href="#contacto">Contacto</a></li>
+          </ul>
+      </div>
+  `;
+
+  return nav;
+}
