87dcdcc608878e2d23df6fe9b4c1f058dc6631b1
matias <matias@ibserveis.com>
2025-05-12 20:48:49 +0200

    importar exportar

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        utils: resolve(__dirname, 'utils.html'),
+      },
+    },
+  },
 });
