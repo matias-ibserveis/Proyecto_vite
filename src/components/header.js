0ceb7c0bdd5dbcce0d8d4f4ca79a596a2ab28f59
VictorCrespi <vcrespiv@alumnes.santjosepobrer.es>
2025-06-02 16:28:18 +0200

    Header mejora

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ export async function Header() {
         //const res = await fetch('http://localhost:3000/productos_portada');
         const res = await fetch('https://proyectorailway-production-9739.up.railway.app/productos_portada');
         const productos = await res.json();
-        //console.log("productos portada", JSON.stringify(productos, null, 2));
+        console.log("productos portada", JSON.stringify(productos, null, 2));
 
         const items = productos.slice(0, 4).map((producto, index) => {
             const imageId = producto.imagen1.split('/d/')[1]?.split('/')[0];
@@ -17,53 +17,38 @@ export async function Header() {
 
             return `
                 <div class="carousel-item ${activeClass}">
-                <img src="${imageUrl}" class="carousel-img" alt="Producto ${index + 1}">
+                    <img src="${imageUrl}" class="d-block w-100" alt="Producto ${index + 1}">
                 </div>
-                 `;
+            `;
         }).join('');
 
-        header.innerHTML = `
-            <div class="contenedor">
-                 <div class="carousel-inner">
-                    ${items}
-                </div>
-                <span class="carousel-btn" data-bs-target="#headerCarousel" data-bs-slide="next"> > </span>
-            </div>
-            <style>
-                .contenedor{
-                    display:flex; 
-                    max-width:1320px; 
-                    margin:1rem ; 
-                    align-items:center;
-                }
-                .carousel-img {
-                    width: 100%;
-                    height: 50vh;
-                    object-fit: cover;
-                    border: 2px solid green;
-                }
-                .carousel-btn {
-                    width: 10%;
-                    background: white;
-                    padding-left: 1rem;
-                    color:green;
-                    border:none;
-                    font-size: 4rem;
-                    cursor: pointer;
-                }
-                
-                @media (max-width: 600px) {
-                    .carousel-btn {
-                        width: 20px;
-                        font-size: 2rem;
-                    }
-                    .container{
-                        margin:2rem 0.5rem 1rem 0.5rem ; 
-                    }
-
-            </style>
-            `;
-
+header.innerHTML = `
+    <div class="carousel-inner">
+        ${items}
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#headerCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#headerCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    <style>
+        .carousel-inner img {
+            height: 50vh;
+            object-fit: cover;
+            width: 100%;
+            border-radius: 10px;
+        }
+        .carousel-control-prev {
+            left: 3%;
+        }
+        .carousel-control-next {
+            right: 3%;
+        }
+    </style>
+`;
     } catch (error) {
         console.error('Error cargando los productos:', error);
     }
